a0833e83bd92e1bce5d17dce929e90690b649ff1
rhymedys <rhymedys@gmail.com>
2019-02-15 17:19:54 +0800

    update had not token result code

--- app/middleware/checkMyDoctorSession.js
+++ app/middleware/checkMyDoctorSession.js
@@ -2,7 +2,7 @@
  * @Author: Rhymedys/Rhymedys@gmail.com
  * @Date: 2018-07-26 10:14:30
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2019-02-13 15:06:20
+ * @Last Modified time: 2019-02-15 17:19:24
  */
 'use strict';
 const response = require('../extend/response');
@@ -15,7 +15,7 @@ module.exports = () => {
     const isValidJSession = JSessionIdInfo && JSessionIdInfo.jSessionId
     console.log('checkMyDoctorSession',isValidJSession)
     if (!isValidJSession) {
-      response.sendFail(ctx, '没有权限访问或权限已过期')
+      response.sendFail(ctx, '没有权限访问或权限已过期',410001)
     } else {
       ctx.state.myDoctorSessionId = isValidJSession
       await next()
